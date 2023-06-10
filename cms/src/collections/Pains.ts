b3412a9172c3c71af22de5c4f0e48cdd452f9c44
Nut Pinyo <bomb.NP@gmail.com>
2023-06-10 16:04:54 +0700

    Add basic entities from Notion (#10)

---
+++ cms/src/collections/Pains.ts
@@ -0,0 +1,27 @@
+import { CollectionConfig } from 'payload/types'
+
+const Pains: CollectionConfig = {
+  slug: 'pains',
+  admin: {
+    useAsTitle: 'problem',
+  },
+  access: {
+    read: () => true,
+  },
+  fields: [
+    {
+      name: 'problem',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'organizations',
+      type: 'relationship',
+      relationTo: 'organizations',
+      hasMany: true,
+    },
+    // TODO: #pain-1 Populate back-reference to Projects,
+  ],
+}
+
+export default Pains
--- cms/src/collections/Projects.ts
+++ cms/src/collections/Projects.ts
@@ -3,26 +3,68 @@ import { CollectionConfig } from 'payload/types'
 const Projects: CollectionConfig = {
   slug: 'projects',
   admin: {
-    useAsTitle: 'title',
+    useAsTitle: 'name',
   },
   access: {
     read: () => true,
   },
   fields: [
     {
-      name: 'title',
+      name: 'name',
       type: 'text',
       required: true,
     },
     {
       name: 'slug',
       type: 'text',
-      required: false,
     },
     {
-      name: 'description',
-      type: 'text',
-      required: false,
+      name: 'status',
+      type: 'select',
+      options: [
+        {
+          label: 'Not Started',
+          value: 'NOT_STARTED',
+        },
+        {
+          label: 'In Development',
+          value: 'IN_DEVELOPMENT',
+        },
+        {
+          label: 'Released',
+          value: 'RELEASED',
+        },
+        {
+          label: 'Done',
+          value: 'DONE',
+        },
+      ],
+      required: true,
+    },
+    {
+      name: 'organizations',
+      type: 'relationship',
+      relationTo: 'organizations',
+      hasMany: true,
+    },
+    {
+      name: 'apis',
+      label: 'APIs',
+      type: 'relationship',
+      relationTo: 'apis',
+      hasMany: true,
+    },
+    {
+      name: 'pains',
+      type: 'relationship',
+      relationTo: 'pains',
+      hasMany: true,
+    },
+    {
+      name: 'hackathons',
+      type: 'relationship',
+      relationTo: 'hackathons',
+      hasMany: true,
     },
   ],
 }
