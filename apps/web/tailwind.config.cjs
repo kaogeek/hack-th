f06458d697baa1f4c5df0ccd6c378081e7f0be7f
Phumrapee Limpianchop <git@rayriffy.com>
2023-06-17 00:33:33 +0900

    chore: staged changes

--- apps/web/tailwind.config.cjs
+++ apps/web/tailwind.config.cjs
@@ -2,7 +2,9 @@ const defaultConfig = require('tailwindcss/defaultConfig')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: [
+    '../../**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
+  ],
   theme: {
     extend: {
       fontFamily: {
